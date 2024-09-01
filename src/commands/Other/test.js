5068c518f3838b89c62fd02c0215565f5d1badf6
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2024-09-01 08:31:29 -0500

    Update test.js

--- src/commands/Other/test.js
+++ src/commands/Other/test.js
@@ -1,15 +1,15 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('test')
-    .setDescription('Test command'),
+        .setName('test')
+        .setDescription('Comando de prueba'),
     async execute(interaction, client) {
 
         const embed = new EmbedBuilder()
-        .setColor("Random")
-        .setDescription(`Test command successful | ${client.user.username} is online!`)
+            .setColor("Random")
+            .setDescription(`Comando de prueba exitoso | ¡${client.user.username} está en línea!`)
 
-        await interaction.reply({ content: `<@${interaction.user.id}>` , embeds: [embed]})
+        await interaction.reply({ content: `<@${interaction.user.id}>`, embeds: [embed] });
     }
-}
+};
