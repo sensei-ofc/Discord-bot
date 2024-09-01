f5f9b4a7fcf542146c0093b1371afdb753edebab
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2024-09-01 08:29:41 -0500

    Update ping.js

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -1,11 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription(`Replies with pong`),
+        .setName('ping')
+        .setDescription('Responde con pong'),
     async execute(interaction) {
-        await interaction.reply({ content: "Sent message in channel", ephemeral: true})
-        await interaction.channel.send({ content: 'Pong!'})
+        await interaction.reply({ content: "Mensaje enviado en el canal", ephemeral: true });
+        await interaction.channel.send({ content: '¡Pong!' });
     }
-}
+};
