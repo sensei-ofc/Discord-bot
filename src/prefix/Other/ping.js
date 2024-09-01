df9d227d563ed4dba6452edf499122e58be4e823
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2024-09-01 08:47:57 -0500

    Update ping.js

--- src/prefix/Other/ping.js
+++ src/prefix/Other/ping.js
@@ -5,12 +5,12 @@ module.exports = {
     async execute(message, client) {
 
         const embed = new EmbedBuilder()
-        .setAuthor({ name: `${client.user.username} ${client.config.devBy}`})
-        .setTitle(`${client.user.username} **ping** command ${client.config.devBy}`)
-        .setDescription(`> Pong! ${client.ws.ping}ms`)
-        .setColor(client.config.embedColor)
-        .setFooter({ text: `Requested by ${message.author.username}`, iconURL: message.author.avatarURL() })
-        .setTimestamp()
+            .setAuthor({ name: `${client.user.username} ${client.config.devBy}`})
+            .setTitle(`${client.user.username} comando **ping** ${client.config.devBy}`)
+            .setDescription(`> ¡Pong! ${client.ws.ping}ms`)
+            .setColor(client.config.embedColor)
+            .setFooter({ text: `Solicitado por ${message.author.username}`, iconURL: message.author.avatarURL() })
+            .setTimestamp();
 
         message.channel.send({ embeds: [embed] });
     }
